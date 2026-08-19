544277a839848f260fb739667650e1875f8bf6d8
impredimex-hub <impredimexiys@gmail.com>
2026-08-19 12:33:55 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/proceso-pwa/',
   plugins: [
     react(),
     VitePWA({
@@ -15,7 +16,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/',
+        start_url: '/proceso-pwa/',
         icons: [
           {
             src: 'https://placehold.co/192x192/0284c7/ffffff.png?text=PROCESO',
